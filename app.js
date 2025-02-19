6cf25fdff5729d320f0298b89a8f4e2d6c16906d
Ana Nogueira <acsnogueira95@gmail.com>
2025-02-18 21:38:40 -0300

    finalizando o abre e fecha da janela de contato

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const contactWindow = document.querySelector('.close-contact');
+const closeBtn = document.querySelector('.close-contact');
+const contactWindowOpened = document.querySelector('.contact-window-open');
+const contactWindowClosed = document.querySelector('.contact-window-closed');
+const contactWindowContainer = document.querySelector('.contact-window');
 
+console.log(closeBtn);
+closeBtn.addEventListener('click', closeContact);
+contactWindowContainer.addEventListener('click', handleClick);
 
-console.log(contactWindow.parentElement);
-
-
-contactWindow.addEventListener('click', closeWindow);
-
-function closeWindow (event) {
-    event.preventDefault();
-    const target = event.currentTarget.parentElement;
-    target.classList.toggle('active');
-    target.nextElementSibling.classList.toggle('active');
+function closeContact(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  contactWindowOpened.classList.remove('active');
+  contactWindowClosed.classList.add('active');
 }
 
-
+function handleClick(event) {
+  event.preventDefault();
+  if (contactWindowClosed.classList.contains('active')) {
+    contactWindowClosed.classList.remove('active');
+    contactWindowOpened.classList.add('active');
+  }
+}
